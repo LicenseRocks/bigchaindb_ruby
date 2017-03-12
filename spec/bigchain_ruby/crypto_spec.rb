003ea914ccf96f0aa1fa8f7770f60ddb5284582d
Adam Groves <adam.groves@gmail.com>
2017-03-12 15:03:11 +0100

    Some keys are shorter than 44 chars

--- spec/bigchain_ruby/crypto_spec.rb
+++ spec/bigchain_ruby/crypto_spec.rb
@@ -6,8 +6,8 @@ describe BigchainRuby::Crypto do
 
     it 'returns a new crypto keypair instance' do
       expect(subject).to be_a(BigchainRuby::Crypto::CryptoKeypair)
-      expect(subject.private_key.length).to eq(44)
-      expect(subject.public_key.length).to eq(44)
+      expect(subject.private_key.length).to be > 40
+      expect(subject.public_key.length).to be > 40
     end
   end
 end
